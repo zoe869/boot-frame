3cb7bf2351ac3168cb2728204d53fcd264f85136
zoe <869736098@qq.com>
2022-07-08 14:46:36 +0100

    crud

--- src/main/java/com/zhong/controller/RecordController.java
+++ src/main/java/com/zhong/controller/RecordController.java
@@ -1,8 +1,12 @@
 package com.zhong.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.zhong.entity.Record;
+import com.zhong.entity.Role;
+import com.zhong.service.RecordService;
+import com.zhong.utils.SysResult;
+import com.zhong.utils.SysResultHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,4 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RecordController {
 
+    @Autowired
+    private RecordService recordService;
+
+    @GetMapping("/{roleId}")
+    public SysResult findById(@PathVariable("roleId") Integer roleId){
+        System.out.println("根据ID查找");
+        return SysResultHandler.success(recordService.findById(roleId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public SysResult deleteById(@PathVariable("roleId") Integer roleId){
+        System.out.println("deleteById");
+        recordService.deleteById(roleId);
+        return SysResultHandler.success();
+    }
+
+    @PutMapping("/{userId}")
+    public SysResult update(@PathVariable("recordId") Integer recordId,@RequestBody Record record){
+        Record res = recordService.findById(recordId);
+        if (res == null) {
+            return SysResultHandler.buildSysResult(404,"not user with id :"+recordId,null);
+        }
+        return SysResultHandler.success(recordService.update(record));
+    }
+
+    @PostMapping
+    public SysResult add(@RequestBody Record record){
+        return SysResultHandler.success(recordService.save(record));
+    }
 }
--- src/main/java/com/zhong/controller/RoleController.java
+++ src/main/java/com/zhong/controller/RoleController.java
@@ -1,8 +1,13 @@
 package com.zhong.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.zhong.entity.Role;
+import com.zhong.entity.User;
+import com.zhong.service.RoleService;
+import com.zhong.service.UserService;
+import com.zhong.utils.SysResult;
+import com.zhong.utils.SysResultHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,4 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RoleController {
 
+    @Autowired
+    private RoleService roleService;
+
+
+
+    @GetMapping("/{roleId}")
+    public SysResult findById(@PathVariable("roleId") Integer roleId){
+        System.out.println("根据ID查找");
+        return SysResultHandler.success(roleService.findById(roleId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public SysResult deleteById(@PathVariable("roleId") Integer roleId){
+        System.out.println("deleteById");
+        roleService.deleteById(roleId);
+        return SysResultHandler.success();
+    }
+
+    @PutMapping("/{userId}")
+    public SysResult update(@PathVariable("userId") Integer userId,@RequestBody Role role){
+        Role res = roleService.findById(userId);
+        if (res == null) {
+            return SysResultHandler.buildSysResult(404,"not user with id :"+userId,null);
+        }
+        return SysResultHandler.success(roleService.update(role));
+    }
+
+    @PostMapping
+    public SysResult add(@RequestBody Role role){
+        return SysResultHandler.success(roleService.save(role));
+    }
 }
